abc5bbb7516c0ab4730ee5e3989aed319c7cb21f
Nick <nschette@yahoo.com>
2015-07-07 11:22:59 -0500

    Commit Worthy

--- global.js
+++ global.js
@@ -1,14 +1,20 @@
 // Javascript Quiz Game
 // Questions
-var q1 = prompt("What is the name of Ned Stark's youngest daughter? A) Yari B) Arya C) Darya D) Catelyn")
-var q2 = prompt("What is the name of the country 'Game of Thrones' takes place in? A) Westeros B) Westrose C) Braavos D) King's Landing")
-var q3 = prompt("Who wields 'Longclaw' out of these characters? A) Jaime Lannister B) The Mountain C) Sandor Clegane D) Jon Snow")
-var q4 = prompt("What is the name of Jon Snow's dire wolf? A) Ghost B) Shaggy C) Scar D) Bloodclaw")
-var q5 = prompt("Which region is Oberyn Martel from? A) Kingdom of the Reach B) Kingdom of the Stormlands C) Principality of Dorne D) Kingdom of the Mountain and the Vale")
+var q1 = {q:"What is the name of Ned Stark's youngest daughter?", options:"1) Yari 2) Arya 3) Darya 4) Catelyn", a: 2};
+var q2 = {q:"What is the name of the country 'Game of Thrones' takes place in?", options:"1) Westeros 2) Westrose 3) Braavos 4) King's Landing", a: 1};
+var q3 = {q:"Who wields 'Longclaw' out of these characters?", options:"1) Jaime Lannister 2) The Mountain 3) Sandor 3legane 4) Jon Snow", a: 4};
+var q4 = {q:"What is the name of Jon Snow's dire wolf?", options:"1) Ghost 2) Shaggy 3) Scar 4) Bloodclaw", a: 1};
+var q5 = {q:"Which region is Oberyn Martel from?", options:"1) Kingdom of the Reach 2) Kingdom of the Stormlands 3) Principality of Dorne 4) Kingdom of the Mountain and the Vale", a: 3};
+// answers -- q1 = 2
+// answers -- q2 = 1
+// answers -- q3 = 4
+// answers -- q4 = 1
+// answers -- q5 = 3
 
-// answers -- q1 = B
-// answers -- q2 = A
-// answers -- q3 = D
-// answers -- q4 = A
-// answers -- q5 = C
+var array = [q1, q2, q3, q4, q5];
+var score, a, i = 0;
 
+for(i = 0; i < array.length; i += 1) {
+  var q = array[i];
+
+}

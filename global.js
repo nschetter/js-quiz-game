2992d026afa87bf1af1596f53ce01fda091ecf37
Nick <nschette@yahoo.com>
2015-07-07 12:13:45 -0500

    Fixed score NaN error

--- global.js
+++ global.js
@@ -3,7 +3,7 @@
 // Questions
 var q1 = {q:"What is the name of Ned Stark's youngest daughter?", options:"1) Yari 2) Arya 3) Darya 4) Catelyn", a: 2};
 var q2 = {q:"What is the name of the country 'Game of Thrones' takes place in?", options:"1) Westeros 2) Westrose 3) Braavos 4) King's Landing", a: 1};
-var q3 = {q:"Who wields 'Longclaw' out of these characters?", options:"1) Jaime Lannister 2) The Mountain 3) Sandor 3legane 4) Jon Snow", a: 4};
+var q3 = {q:"Who wields 'Longclaw' out of these characters?", options:"1) Jaime Lannister 2) The Mountain 3) Sandor Clegane 4) Jon Snow", a: 4};
 var q4 = {q:"What is the name of Jon Snow's dire wolf?", options:"1) Ghost 2) Shaggy 3) Scar 4) Bloodclaw", a: 1};
 var q5 = {q:"Which region is Oberyn Martel from?", options:"1) Kingdom of the Reach 2) Kingdom of the Stormlands 3) Principality of Dorne 4) Kingdom of the Mountain and the Vale", a: 3};
 var q6 = {q:"Which of these characters killed their mother during childbirth", options:"1)Petyr Baelish 2) Ser Jorah 3) Tyrion Lannister 4) Shireen Baratheon", a: 3};
@@ -24,7 +24,8 @@ var q10 = {q:"The common expression in High Valyrian 'Valar Morghulis' translate
 
 // establishes the question array and creates our variables
 var qarray = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10];
-var score, a, i = 0;
+var a, i = 0;
+var score = 0;
 
 alert("Get ready for a Quiz Game of Thrones! Don't worry -- these are only season/book one questions, so no there are spoilers.");
 
@@ -43,7 +44,8 @@ for (i = 0; i < qarray.length; i += 1) {
 }
 
 // calculates the percent by dividing the score by the number of questions and multiplying by 100
-var percent = ((score/qarray.length) * 100);
+var numberOfQuestions = parseInt(qarray.length);
+var percent = ((score/numberOfQuestions) * 100);
 alert("You got " + percent + "% of the questions right, and you need 70% to pass");
 
 // If the score is 70% or higher, then the user passes

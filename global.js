88907c902665b723e5e661d4f31be4af6890cb7b
Nick <nschette@yahoo.com>
2015-07-07 11:41:39 -0500

    Adding more q's

--- global.js
+++ global.js
@@ -5,16 +5,44 @@ var q2 = {q:"What is the name of the country 'Game of Thrones' takes place in?",
 var q3 = {q:"Who wields 'Longclaw' out of these characters?", options:"1) Jaime Lannister 2) The Mountain 3) Sandor 3legane 4) Jon Snow", a: 4};
 var q4 = {q:"What is the name of Jon Snow's dire wolf?", options:"1) Ghost 2) Shaggy 3) Scar 4) Bloodclaw", a: 1};
 var q5 = {q:"Which region is Oberyn Martel from?", options:"1) Kingdom of the Reach 2) Kingdom of the Stormlands 3) Principality of Dorne 4) Kingdom of the Mountain and the Vale", a: 3};
+var q6 = {q:"Which of these characters killed their mother during childbirth", options:"1)Petyr Baelish 2) Ser Jorah 3) Tyrion Lannister 4) Shireen Baratheon", a: 3};
+var q7 = {q:""}
+var q8 = {q:""}
+var q9 = {q:""}
+var q10 = {q:""}
 // answers -- q1 = 2
 // answers -- q2 = 1
 // answers -- q3 = 4
 // answers -- q4 = 1
 // answers -- q5 = 3
+// answers -- q6 = 3
+// answers -- q7 = 
+// answers -- q8 = 
+// answers -- q9 = 
+// answers -- q10 = 
 
-var array = [q1, q2, q3, q4, q5];
+var qarray = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10];
 var score, a, i = 0;
 
-for(i = 0; i < array.length; i += 1) {
-  var q = array[i];
+alert("Get ready for a Quiz Game of Thrones! Don't worry -- these are only season/book one questions, so no there are spoilers.");
 
+for (i = 0; i < qarray.length; i += 1) {
+  var q = qarray[i];
+  alert(q.q);
+  a = parseInt(prompt(q.options));
+  if (a == q.a) {
+    alert("That's right!");
+    score += 1;
+  } else {
+    alert("Wrong answer...");
+  }
+}
+
+var percent = ((score/qarray.length) * 100);
+alert("You got " + percent + "% of the questions right, and you need 70% to pass");
+
+if (percent >= 70) {
+  alert("You know a good amount about 'Game of Thrones'; you passed!")
+} else {
+  alert("You know nothing Jon Snow; you failed")
 }

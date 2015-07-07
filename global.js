859362e05f0ea246cd9c03c2403ef2fa65870d71
Nick <nschette@yahoo.com>
2015-07-07 11:03:20 -0500

    I had too much fun typing the questions out

---
+++ global.js
@@ -0,0 +1,14 @@
+// Javascript Quiz Game
+// Questions
+var q1 = prompt("What is the name of Ned Stark's youngest daughter? A) Yari B) Arya C) Darya D) Catelyn")
+var q2 = prompt("What is the name of the country 'Game of Thrones' takes place in? A) Westeros B) Westrose C) Braavos D) King's Landing")
+var q3 = prompt("Who wields 'Longclaw' out of these characters? A) Jaime Lannister B) The Mountain C) Sandor Clegane D) Jon Snow")
+var q4 = prompt("What is the name of Jon Snow's dire wolf? A) Ghost B) Shaggy C) Scar D) Bloodclaw")
+var q5 = prompt("Which region is Oberyn Martel from? A) Kingdom of the Reach B) Kingdom of the Stormlands C) Principality of Dorne D) Kingdom of the Mountain and the Vale")
+
+// answers -- q1 = B
+// answers -- q2 = A
+// answers -- q3 = D
+// answers -- q4 = A
+// answers -- q5 = C
+
